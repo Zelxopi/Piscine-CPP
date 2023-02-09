749b64fb88ffe8e5920ed660ffe94f8e740ef544
Maya Tremblay <mtrembla@student.42quebec.com>
2023-02-09 14:55:08 -0500

    megaphone

--- module00/ex01/phonebook.cpp
+++ module00/ex01/phonebook.cpp
@@ -1,30 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   phonebook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mtrembla <mtrembla@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/09 12:25:03 by mtrembla          #+#    #+#             */
-/*   Updated: 2023/02/09 12:25:06 by mtrembla         ###   ########.fr       */
+/*   Created: 2023/02/09 14:50:29 by mtrembla          #+#    #+#             */
+/*   Updated: 2023/02/09 14:50:32 by mtrembla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main(int argc, char **argv)
+int main(void)
 {
-    char c;
-    
-	for(int i = 1; argv[i]; i++) {
-    	for (int j = 0; j < strlen(argv[i]); j++) {
-			c = toupper(argv[i][j]);
-			std::cout << c;
-		}
-    	std::cout << " ";
-	}
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	std::cout << std::endl;
     return(0);
 }
